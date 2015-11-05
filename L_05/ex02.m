98614ae2554a75ec9a1c2dc8ed2a9987db1cf870
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-11-05 19:58:24 +0000

    L_05

---
+++ L_05/ex02.m
@@ -0,0 +1,32 @@
+T = [0.7, 0.2, 0.3
+     0.2, 0.3, 0.3
+     0.1, 0.5, 0.4];
+
+X_0 = [1, 0, 0]';
+
+P_2 = T ^ 2 * X_0;
+fprintf("P_2(Chuva) = %.2f\n", P_2(3));
+
+N = 20;
+T_n = T(:);
+helper = T;
+for i = 2:N
+	helper = helper * T;
+	T_n = [T_n, helper(:)];
+end
+
+plot(T_n');
+
+T_n_2 = T(:);
+helper = T;
+epsilon = 10^-4
+i = 2;
+while 1
+	helper = helper * T;
+	T_n_2 = [T_n_2, helper(:)];
+	if abs(T_n_2(i) - T_n_2(i - 1)) < epsilon
+		break;
+	end
+	i = i + 1;
+end
+
---
+++ L_05/ex03.m
@@ -0,0 +1,53 @@
+N = 20;
+
+T =  rand(N);
+T = bsxfun(@rdivide, T, sum(T));
+CumSumT = cumsum(T);
+
+J = 20;
+c = 0;
+t = 1e3;
+for j = 1:t
+	X = [ceil(N * rand), zeros(1, J)];
+	for i = 2:J + 1
+		X(i) = find(CumSumT(:, X(i-1)) > rand)(1);
+	end
+	for i = 2:J
+		if X(i-1) == 1 && X(i) == N
+			c = c + 1;
+		end
+	end
+end
+fprintf("P(1 -> %d in %d trials) = %.2f\n", N, J, c / t);
+ 
+J = 40;
+c = 0;
+t = 1e3;
+for j = 1:t
+        X = [ceil(N * rand), zeros(1, J)];
+        for i = 2:J + 1
+                X(i) = find(CumSumT(:, X(i-1)) > rand)(1);
+        end
+        for i = 2:J
+                if X(i-1) == 1 && X(i) == N
+                        c = c + 1;
+                end
+        end
+end
+fprintf("P(1 -> %d in %d trials) = %.2f\n", N, J, c / t);
+
+J = 100;
+c = 0;
+t = 1e3;
+for j = 1:t
+        X = [ceil(N * rand), zeros(1, J)];
+        for i = 2:J + 1
+                X(i) = find(CumSumT(:, X(i-1)) > rand)(1);
+        end
+        for i = 2:J
+                if X(i-1) == 1 && X(i) == N
+                        c = c + 1;
+                end
+        end
+end
+fprintf("P(1 -> %d in %d trials) = %.2f\n", N, J, c / t);
